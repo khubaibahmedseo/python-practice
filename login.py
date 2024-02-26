d56248a6774d5b69d53f174a8258ea116ff4af40
khubaibahmedseo <khubaibahmed127@gmail.com>
2024-02-27 03:25:10 +0500

    initial

--- login.py
+++ login.py
@@ -1,20 +1,36 @@
 from tkinter import *
 import customtkinter
+# from PIL import ImageTk, Image
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
-root =customtkinter.CTk()
-root.title('Tkinter.com - Custom Tkinter Buttons')
-# root.iconbitmap('images/codemy.ico')
-root.geometry('600x350')
+app = customtkinter.CTk()
+app.title('Login')
+app.geometry('600x440')
 
-def hello():
-    print("Hello")
+l1 = customtkinter.CTkLabel(master=app)
+l1.pack()  
 
+frame = customtkinter.CTkFrame(master=l1, width=320, height=360, corner_radius=15)
+frame.grid(row=0, column=0, pady=30)
 
-my_button = customtkinter.CTkButton(root, text="Hello world",command=hello)
-my_button.grid(row = 0 , column = 0 ,pady=80)
-my_button_1 = customtkinter.CTkButton(root, text="Hello world",command=hello)
-my_button_1.grid(row = 0 , column = 1,pady=5)
-root.mainloop()
+l2=customtkinter.CTkLabel(master=frame, text="Login into your Account", font=('century Gothic', 20))
+l2.place(x=50,y=45)
+
+entry1 = customtkinter.CTkEntry(master=frame, width=220, placeholder_text="Username")
+entry1.place(x=50,y=110)
+
+entry2 = customtkinter.CTkEntry(master=frame, width=220, placeholder_text="Password")
+entry2.place(x=50,y=165)
+
+l2=customtkinter.CTkLabel(master=frame, text="Forget Password", font=('century Gothic', 12))
+l2.place(x=165,y=195)
+
+button1 = customtkinter.CTkButton(master=frame, width=220, text='Login', corner_radius=6)
+button1.place(x=50,y=240)
+
+button2 = customtkinter.CTkButton(master=frame, width=220, text='Sign up', corner_radius=6)
+button2.place(x=50,y=280)
+
+app.mainloop()
